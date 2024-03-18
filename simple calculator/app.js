20a1942c0c88f747e54d39fc7a064d2a5ef3b919
zainabafzalshaikh <161166927+zainabafzalshaikh@users.noreply.github.com>
2024-03-17 18:13:09 -0700

    Add files via upload

---
+++ simple calculator/app.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter First number", type: "number", name: "FirstNumber" },
+    { message: "Enter Second number", type: "number", name: "SecondNumber" },
+    {
+        message: "Select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+    },
+]);
+// conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.FirstNumber + answer.SecondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.FirstNumber - answer.SecondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.FirstNumber / answer.SecondNumber);
+}
+else {
+    console.log("please select valid operator");
+}
